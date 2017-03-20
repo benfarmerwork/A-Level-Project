ae181d8d8413c1914f18a04b09857a1122a5f507
Ben Farmer <ben_farmer3@hotmail.com>
2017-03-20 10:12:38 +0000

    Add files via upload

---
+++ WeatherStationAlevelProject/WeatherStationAlevelProject/WeatherStation.ClientUploader/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStation.ClientUploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var weatherserviceclient = new WeatherService.WeatherServiceClient();
+            DateTime? LastLog = weatherserviceclient.GetMostRecentWeatherLogDateTime(3);
+
+            Console.WriteLine("Last Log : " + LastLog.Value.ToString());
+            Console.ReadLine();
+        }
+    }
+}
